f2300e7f8c36151f223d179a8f2328732c650a8a
mostafa <56946868+symtyx@users.noreply.github.com>
2022-03-12 01:27:17 -0500

    Add files via upload

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Mostafa Atmar on 3/11/22.
+//  Copyright © 2022 com.codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
